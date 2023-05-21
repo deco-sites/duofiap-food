a1fdf99799cd6ef3e7e3b5f2cdd5eefff6eee84e
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-05-21 12:13:56 -0300

    all upgrades

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -13,7 +13,7 @@ function MenuItem({ item }: { item: INavItem }) {
       <div class="collapse-content">
         <ul>
           <li>
-            <a class="underline text-sm" href={item.href}>Ver todos</a>
+            <a class="underline text-sm" href={item.href}>View all</a>
           </li>
           {item.children?.map((node) => (
             <li>
@@ -35,45 +35,6 @@ function Menu({ items }: Props) {
             <MenuItem item={item} />
           </li>
         ))}
-      </ul>
-
-      <ul class="flex flex-col py-2 bg-base-200">
-        <li>
-          <a
-            class="flex items-center gap-4 px-4 py-2"
-            href="/wishlist"
-          >
-            <Icon id="Heart" width={20} height={20} strokeWidth={2} />
-            <span class="text-sm">Lista de desejos</span>
-          </a>
-        </li>
-        <li>
-          <a
-            class="flex items-center gap-4 px-4 py-2"
-            href="https://www.deco.cx"
-          >
-            <Icon id="MapPin" width={20} height={20} strokeWidth={2} />
-            <span class="text-sm">Nossas lojas</span>
-          </a>
-        </li>
-        <li>
-          <a
-            class="flex items-center gap-4 px-4 py-2"
-            href="https://www.deco.cx"
-          >
-            <Icon id="Phone" width={20} height={20} strokeWidth={2} />
-            <span class="text-sm">Fale conosco</span>
-          </a>
-        </li>
-        <li>
-          <a
-            class="flex items-center gap-4 px-4 py-2"
-            href="https://www.deco.cx"
-          >
-            <Icon id="User" width={20} height={20} strokeWidth={2} />
-            <span class="text-sm">Minha conta</span>
-          </a>
-        </li>
       </ul>
     </>
   );
